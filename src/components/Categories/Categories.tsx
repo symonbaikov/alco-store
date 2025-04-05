1375d1a4a8599ccf1b2393a52a83ff957fdae40f
MostOfLuck <peterbaikov12@gmail.com>
2025-04-05 18:45:47 +0300

    Обновлены компоненты Categories и Slider: изменены изображения и тексты на болгарском языке, исправлены стили для улучшения отображения. Уменьшен интервал смены слайдов до 3000 мс. Удалены лишние отступы в Home.css.

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -7,31 +7,34 @@ const Categories: React.FC = () => {
       <div className="categories-container">
         <div className="category-card">
           <span className="category-label">УСЛУГА</span>
-          <img src="/images/categories/wine-tasting.jpg" alt="Дегустация вин" />
+          <img
+            src="/images/mir4hh1fc4wnc9c678752h2z6j0dpd8k.jpg"
+            alt="Дегустация на вина"
+          />
           <div className="category-content">
-            <h3>Дегустация вин и сыров Италии</h3>
+            <h3>Дегустация на вина и сирена от Италия</h3>
           </div>
         </div>
 
         <div className="category-card">
           <span className="category-label">БЛОГ</span>
           <img
-            src="/images/categories/cocktails.jpg"
-            alt="Освежающие напитки"
+            src="/images/1usfz2uztycqbh8eupl35fb1xmlro1x4.jpg"
+            alt="Освежаващи напитки"
           />
           <div className="category-content">
-            <h3>Освежающие и прохладные напитки</h3>
+            <h3>Освежаващи и студени напитки</h3>
           </div>
         </div>
 
         <div className="category-card">
-          <span className="category-label">АКЦИЯ</span>
+          <span className="category-label">ПРОМОЦИЯ</span>
           <img
-            src="/images/categories/special-offer.jpg"
-            alt="Спецпредложения"
+            src="/images/ygye22g21kty21m5vmrz24lqv97zqe2v.jpg"
+            alt="Специални предложения"
           />
           <div className="category-content">
-            <h3>Актуальные спецпредложения на любимые напитки</h3>
+            <h3>Актуални специални предложения за любими напитки</h3>
           </div>
         </div>
       </div>
