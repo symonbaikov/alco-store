a032f868514e9aeac8abb14c7b3d717b715b1d6d
MostOfLuck <peterbaikov12@gmail.com>
2025-04-10 19:04:38 +0300

    Обновлены стили компонентов AuthPage, Categories, DrinkCategories, Features и PopularCategories: изменены отступы, размеры и добавлены новые классы для улучшения адаптивности и визуального восприятия. Исправлены тексты и добавлены изображения для категорий.

--- src/components/PopularCategories/PopularCategories.tsx
+++ src/components/PopularCategories/PopularCategories.tsx
@@ -1,51 +1,57 @@
 import React from "react";
 import "./PopularCategories.css";
-import WineBarIcon from "@mui/icons-material/WineBar";
-import LocalBarIcon from "@mui/icons-material/LocalBar";
-import LiquorIcon from "@mui/icons-material/Liquor";
-import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
-import SportsBarIcon from "@mui/icons-material/SportsBar";
-import RestaurantIcon from "@mui/icons-material/Restaurant";
 
 const PopularCategories: React.FC = () => {
   return (
     <section className="popular-categories">
-      <h2 className="section-title">Популярни категории</h2>
+      <h2 className="section-title">Популярни категории стоки</h2>
       <div className="categories-grid">
         <div className="category-item">
-          <div className="icon-wrapper">
-            <WineBarIcon className="category-icon" />
-          </div>
+          <img
+            src="/images/categories-wine.png"
+            alt="Вино"
+            className="category-image"
+          />
           <span>Вино</span>
         </div>
         <div className="category-item">
-          <div className="icon-wrapper">
-            <LocalBarIcon className="category-icon" />
-          </div>
+          <img
+            src="/images/categories-whiskey.jpg"
+            alt="Високоалкохолни напитки"
+            className="category-image"
+          />
           <span>Високоалкохолни напитки</span>
         </div>
         <div className="category-item">
-          <div className="icon-wrapper">
-            <LiquorIcon className="category-icon" />
-          </div>
-          <span>Ликьори</span>
+          <img
+            src="/images/categories-liquor.jpg"
+            alt="Ликьори, вермути"
+            className="category-image"
+          />
+          <span>Ликьори, вермути</span>
         </div>
         <div className="category-item">
-          <div className="icon-wrapper">
-            <LocalDrinkIcon className="category-icon" />
-          </div>
+          <img
+            src="/images/categories-drinks.jpg"
+            alt="Напитки"
+            className="category-image"
+          />
           <span>Напитки</span>
         </div>
         <div className="category-item">
-          <div className="icon-wrapper">
-            <SportsBarIcon className="category-icon" />
-          </div>
+          <img
+            src="/images/categories-beer.jpg"
+            alt="Бира"
+            className="category-image"
+          />
           <span>Бира</span>
         </div>
         <div className="category-item">
-          <div className="icon-wrapper">
-            <RestaurantIcon className="category-icon" />
-          </div>
+          <img
+            src="/images/categories-snacks.jpg"
+            alt="Мезета"
+            className="category-image"
+          />
           <span>Мезета</span>
         </div>
       </div>
